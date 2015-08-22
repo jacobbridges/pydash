4e6250ca776579ec237ec02cd1c877603f6ac1f7
jacobbridges <him@jacobandkate143.com>
2015-08-22 15:04:49 -0400

    added performance tests for the following pydash functions
    pydash.array.interleave
    pydash.array.intersection
    pydash.array.intersperse
    pydash.array.last
    pydash.array.last_index_of
    pydash.array.mapcat
    pydash.array.object_
    pydash.array.pull
    pydash.array.pull_at
    pydash.array.remove
    pydash.array.rest
    pydash.array.reverse
    pydash.array.shift
    pydash.array.slice_

--- perf/perftest.py
+++ perf/perftest.py
@@ -294,6 +294,192 @@ def test_python_intercalate():
     assert _l == [1, 'x', 2, 'x', 3, 4, 'x', [5, 6]]
 
 
+def test_pydash_interleave():
+    from pydash.arrays import interleave
+    l = ([1, 2, 3], [4, 5, 6], [7, 8, 9, 10], [1, 2])
+    assert interleave(*l) == [1, 4, 7, 1, 2, 5, 8, 2, 3, 6, 9, 10]
+
+
+def test_python_interleave():
+    from itertools import cycle, islice
+    l = ([1, 2, 3], [4, 5, 6], [7, 8, 9, 10], [1, 2])
+    def roundrobin(*iterables):
+        """Pulled from https://docs.python.org/2/library/itertools.html#recipes"""
+        pending = len(iterables)
+        nexts = cycle(iter(it).__next__ for it in iterables)
+        while pending:
+            try:
+                for next in nexts:
+                    yield next()
+            except StopIteration:
+                pending -= 1
+                nexts = cycle(islice(nexts, pending))
+    assert list(roundrobin(*l)) == [1, 4, 7, 1, 2, 5, 8, 2, 3, 6, 9, 10]
+
+
+def test_pydash_intersection():
+    from pydash.arrays import intersection
+    l = ([1, 2, 3], [4, 5, 6, 2, 3], [0, 0, 1, 2, 3, 4])
+    assert intersection(*l) == [2, 3]
+
+
+def test_python_intersection():
+    l = ([1, 2, 3], [4, 5, 6, 2, 3], [0, 0, 1, 2, 3, 4])
+    _l = set(l[0])
+    for s in [set(a) for a in l[1:]]:
+        _l = _l.intersection(s)
+    assert list(_l) == [2, 3]
+
+
+def test_pydash_intersperse():
+    from pydash.arrays import intersperse
+    l = [1, 2, 3, [4]]
+    assert intersperse(l, 'x') == [1, 'x', 2, 'x', 3, 'x', [4]]
+
+
+def test_python_intersperse():
+    l = [1, 2, 3, [4]]
+    _l = []
+    for v in l[:-1]:
+        _l.append(v)
+        _l.append('x')
+    _l.append(l[-1])
+    assert _l == [1, 'x', 2, 'x', 3, 'x', [4]]
+
+
+def test_pydash_last():
+    from pydash.arrays import last
+    l = [1, 2, 3, 4]
+    assert last(l) == 4
+
+
+def test_python_last():
+    l = [1, 2, 3, 4]
+    assert l[-1] == 4
+
+
+def test_pydash_last_index_of():
+    from pydash.arrays import last_index_of
+    l = [1, 2, 3, 4, 3]
+    assert last_index_of(l, 3) == 4
+
+
+def test_python_last_index_of():
+    l = [1, 2, 3, 4, 3]
+    assert len(l) - l[::-1].index(3) - 1 == 4
+
+
+def test_pydash_mapcat():
+    from pydash.arrays import mapcat
+    l = [1, 2, 3, 4]
+    assert mapcat(l, lambda x: list(range(x))) == [0, 0, 1, 0, 1, 2, 0, 1, 2, 3]
+
+
+def test_python_mapcat():
+    l = [1, 2, 3, 4]
+    _l = []
+    for v in l:
+        _l += list(range(v))
+    assert _l == [0, 0, 1, 0, 1, 2, 0, 1, 2, 3]
+
+
+def test_pydash_object_():
+    from pydash.arrays import object_
+    keys = [1, 2, 3]
+    values = [4, 5, 6]
+    assert object_(keys, values) == {1: 4, 2: 5, 3: 6}
+
+
+def test_python_object_():
+    keys = [1, 2, 3]
+    values = [4, 5, 6]
+    assert dict(zip(keys, values)) == {1: 4, 2: 5, 3: 6}
+
+
+def test_pydash_pull():
+    from pydash.arrays import pull
+    l = [1, 2, 3, 4]
+    pull(l, 2, 4)
+    assert l == [1, 3]
+
+
+def test_python_pull():
+    l = [1, 2, 3, 4]
+    l = [v for v in l if v not in [2, 4]]
+    assert l == [1, 3]
+
+
+def test_pydash_pull_at():
+    from pydash.arrays import pull_at
+    l = [1, 2, 3, 4]
+    pull_at(l, 0, 2)
+    assert l == [2, 4]
+
+
+def test_python_pull_at():
+    l = [1, 2, 3, 4]
+    l = [v for k, v in enumerate(l) if k not in [0, 2]]
+    assert l == [2, 4]
+
+
+def test_pydash_remove():
+    from pydash.arrays import remove
+    l = [1, 2, 3, 4]
+    remove(l, lambda x: x < 3)
+    assert l == [3, 4]
+
+
+def test_python_remove():
+    l = [1, 2, 3, 4]
+    for v in l[::]:
+        if v < 3:
+            l.remove(v)
+    assert l == [3, 4]
+
+
+def test_pydash_rest():
+    from pydash.arrays import rest
+    l = [1, 2, 3, 4]
+    assert rest(l) == [2, 3, 4]
+
+
+def test_python_rest():
+    l = [1, 2, 3, 4]
+    assert l[1:] == [2, 3, 4]
+
+
+def test_pydash_reverse():
+    from pydash.arrays import reverse
+    l = [1, 2, 3, 4]
+    assert reverse(l) == [4, 3, 2, 1]
+
+
+def test_python_reverse():
+    l = [1, 2, 3, 4]
+    assert list(reversed(l)) == [4, 3, 2, 1]
+
+
+def test_pydash_shift():
+    from pydash.arrays import shift
+    l = [1, 2, 3, 4]
+    assert shift(l) == 1 and l == [2, 3, 4]
+
+
+def test_python_shift():
+    l = [1, 2, 3, 4]
+    assert l.pop(0) == 1 and l == [2, 3, 4]
+
+
+def test_pydash_slice_():
+    from pydash.arrays import slice_
+    l = [1, 2, 3, 4]
+    assert slice_(l, 0, 2) == [1, 2]
+
+
+def test_python_slice_():
+    l = [1, 2, 3, 4]
+    assert l[0:2] == [1, 2]
+
 
 def main():
     perftests = [
@@ -316,6 +502,20 @@ def main():
         TimedTest('pydash.array.index_of', 'test_python_index_of', 'test_pydash_index_of'),
         TimedTest('pydash.array.initial', 'test_python_initial', 'test_pydash_initial'),
         TimedTest('pydash.array.intercalate', 'test_python_intercalate', 'test_pydash_intercalate'),
+        TimedTest('pydash.array.interleave', 'test_python_interleave', 'test_pydash_interleave'),
+        TimedTest('pydash.array.intersection', 'test_python_intersection', 'test_pydash_intersection'),
+        TimedTest('pydash.array.intersperse', 'test_python_intersperse', 'test_pydash_intersperse'),
+        TimedTest('pydash.array.last', 'test_python_last', 'test_pydash_last'),
+        TimedTest('pydash.array.last_index_of', 'test_python_last_index_of', 'test_pydash_last_index_of'),
+        TimedTest('pydash.array.mapcat', 'test_python_mapcat', 'test_pydash_mapcat'),
+        TimedTest('pydash.array.object_', 'test_python_object_', 'test_pydash_object_'),
+        TimedTest('pydash.array.pull', 'test_python_pull', 'test_pydash_pull'),
+        TimedTest('pydash.array.pull_at', 'test_python_pull_at', 'test_pydash_pull_at'),
+        TimedTest('pydash.array.remove', 'test_python_remove', 'test_pydash_remove'),
+        TimedTest('pydash.array.rest', 'test_python_rest', 'test_pydash_rest'),
+        TimedTest('pydash.array.reverse', 'test_python_reverse', 'test_pydash_reverse'),
+        TimedTest('pydash.array.shift', 'test_python_shift', 'test_pydash_shift'),
+        TimedTest('pydash.array.slice_', 'test_python_slice_', 'test_pydash_slice_'),
     ]
     print('{0: <30} | {1: ^8} | {2: ^8} |'.format('Test Name', 'pydash', 'python'))
     print('{0: <30} | {1: <8} | {2: <8} |'.format('---------', 'time(ms)', 'time(ms)'))
